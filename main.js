35ff966f7cfe70d98c61d375220eb72559bfb3d3
Swapnil-Srivastava273 <ss.bluescreenofd273@gmail.com>
2022-04-10 20:57:30 +0530

    Kindof initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+"use strict"
+const main=document.getElementById("main");
+const ctx=main.getContext("2d");
+main.height=innerHeight;
+main.width=innerWidth;
+
+// TBD: Background gradients, boards, buttons
+// Gotta initiate all the towers
+// Animation random or coordinated?
+// Gotta find some beautiful gradients
+// So, what tower size to pick?
+class Tower{
+    // Height, position , animation , item.
+    // Isometric angles : width fixed.
+    // Update, animate, z index adjustment, precise center location
+
+}
