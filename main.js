318f5f9418380ffecd50636917551775721d96f5
Swapnil-Srivastava273 <ss.bluescreenofd273@gmail.com>
2022-04-12 17:30:35 +0530

    Boards work

--- main.js
+++ main.js
@@ -17,20 +17,62 @@ let tempside=main.height/10;
 let tempcols=Math.ceil(main.width/(tempside*cos30));
 let side=main.width/((tempcols-1)*cos30);
 let towers=[];
+let boardWidth=side*cos30*1.25;
+let boardHeight=boardWidth*94/128;
+let poleHeight=boardHeight;
+let imgContent=0;
+main.style.zIndex=tempcols*10+1;
+class Boards{
+    // x,y,element
+    constructor(x,y,tower){
+        this.x=x;
+        this.y=y;
+        this.tower=tower;
+        this.element=document.createElement("div");
+        this.element.classList.add("board");
+        this.element.style.width=`${boardWidth}px`;
+        this.element.style.height=`${boardHeight}px`;
+        this.element.style.zIndex=tempcols-this.y;
+        this.img=document.createElement("img");
+        this.img.src=images[imgContent];
+        this.element.appendChild(this.img);
+        //this.element.style.visibility='hidden';
+        
+        document.body.appendChild(this.element);
+
+    }
+    draw(){
+        let cx=this.x*side*cos30,cy=main.height-this.y*side/2-this.tower.height;
+        ctx.lineWidth=1;
+        ctx.beginPath();
+        ctx.moveTo(cx,cy);
+        ctx.lineTo(cx,cy-poleHeight);
+        ctx.closePath();
+        ctx.stroke();
+        ctx.clearRect(cx-boardWidth/2,cy-poleHeight-boardHeight,boardWidth,boardHeight);
+        ctx.strokeRect(cx-boardWidth/2,cy-poleHeight-boardHeight,boardWidth,boardHeight);
+        ctx.lineWidth=0.1;
+        this.element.style.left=`${cx-boardWidth/2}px`;
+        this.element.style.top=`${cy-poleHeight-boardHeight}px`
+    }
+
+}
 class Tower{
     // Height, position , animation , item.
     // Isometric angles : width fixed.
     // Draw, animate, z index adjustment, precise center location
-    constructor(height,x,y,item){
+    constructor(height,x,y){
         this.height=height;
         // x and y assume origin at bottom left
         this.x=x;
         this.y=y;
-        this.item=item;
+        this.item=null;
     }
     draw(){
         let cx=this.x*side*cos30,cy=main.height-this.y*side/2-this.height;
         // Top
+        debugger
+        ctx.lineWidth=0.1;
         ctx.beginPath();
         ctx.moveTo(cx-side*cos30,cy);
         ctx.lineTo(cx,cy-side/2);
@@ -60,6 +102,9 @@ class Tower{
         ctx.fillStyle=rightG;
         ctx.fill();
         ctx.stroke();
+        if(this.item){
+            this.item.draw();
+        }
     }
 
 }
@@ -70,7 +115,10 @@ for(let i=0;i<tempcols;i++){
             towers.push([]);
         }
         if((i+j)%2==0){
-            towers[i].push(new Tower(Math.random()*(main.height-side*5),i,j));
+            let tow=new Tower(Math.random()*(main.height-side*5),i,j);
+            let bo=new Boards(i,j,tow);
+            tow.item=bo;
+            towers[i].push(tow);
             // towers[i].push(new Tower(0,i,j));
             
         }else{
